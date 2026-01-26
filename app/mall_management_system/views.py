1defb57f2a12dd2fe835e06163e902bcba48d69d
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-26 20:41:32 +0545

    updated

--- app/mall_management_system/views.py
+++ app/mall_management_system/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>This is mall Management System index page</h1>")
+    return render(request, 'dashboard/dashboard.html')
