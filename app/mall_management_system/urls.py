8ee906d83eed052aa2399fc0a935fe0f29bfb753
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-26 21:08:55 +0545

    created a url for dashboard

--- app/mall_management_system/urls.py
+++ app/mall_management_system/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'mall_management_system'
 
 urlpatterns = [
-    path ('',views.index, name="index")
+    path ('',views.index, name="index"),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
