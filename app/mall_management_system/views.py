fdedc13c2bd0a7877582148345037ab848e585a0
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-26 21:09:26 +0545

    added dashboard

--- app/mall_management_system/views.py
+++ app/mall_management_system/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 
 def index(request):
+    return render(request, 'index.html')
+
+def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
