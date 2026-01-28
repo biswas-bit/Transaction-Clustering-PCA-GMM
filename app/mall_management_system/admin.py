bed1ad7a788f2402028ddf4fde9ca4502854f208
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-28 20:07:59 +0545

    created admin panel for that models

--- app/mall_management_system/admin.py
+++ app/mall_management_system/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import *
 
-# Register your models here.
+@admin.register(store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'store_id', 'category', 'location', 'status', 'created_at')
+    list_filter = ('category', 'location', 'status')
+    search_fields = ('name', 'store_id', 'owner')
+    ordering = ('-created_at',)
