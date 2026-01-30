018e2464e6611398341bd161b670e6f27fdbeba5
biswas-bit <pokhrelbiswas53@gmail.com>
2026-01-30 21:10:01 +0545

    back to normal urls

--- app/mall_management_system/urls.py
+++ app/mall_management_system/urls.py
@@ -10,10 +10,5 @@ urlpatterns = [
     path('sales/',views.sales, name='sales'),
     path('customers/',views.customers, name='customers'),
     path('inventory/',views.inventory, name='inventory'),
-    path('api/stores/', views.api_stores, name='api_stores'),
-     path('api/stores/<str:store_id>/', views.api_store_detail, name='api_store_detail'),
-    path('api/store-stats/', views.api_store_stats, name='api_store_stats'),
-     path('api/top-stores/', views.api_top_stores, name='api_top_stores'),
-     path('api/lease-timeline/', views.api_lease_timeline, name='api_lease_timeline'),
-    
+   
 ]
