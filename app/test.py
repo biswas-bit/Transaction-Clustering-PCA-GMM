4fed9850a09366a608ae5069be9b8e10f5830fb2
biswas-bit <pokhrelbiswas53@gmail.com>
2026-03-10 21:23:07 +0545

    updated

--- app/test.py
+++ app/test.py
@@ -1,4 +1,11 @@
-from .models import Transaction
+from models import Segmenter
+import numpy as np
 
-all_data = Transaction.objects.all()
-print(all_data)
+data = np.array([2,200,13,3])  
+cluster = Segmenter('app/models/Customer_Segmentation_v1.pkl')
+predictions = cluster.get_cluster(data)
+proba = cluster.get_probabilities(data)
+print(predictions)
+print(proba)
+input("press to exit..")
+    
